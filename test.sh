8c336ead3e214933a29bfe56b65e8144e0de96ed
gsharp <gsharp.edu.cn>
2019-11-15 16:31:16 +0800

    test changed

--- test.sh
+++ test.sh
@@ -2,16 +2,16 @@
 export GOPATH=$PWD
 cd ./src/raft
 if [ $# > 0 ]
+then
     if [ $0 == '/?' -o $0 == '-h' -o  $0 == '--help' ]
         echo -e 'Type ./run.sh 1 for Assignment_1 test.\n.Type /run.sh 2 for Assignment_2 test, you will get two pass if everything have done alright.\n
         Default no parameter for both two tests'
-    else
-        if [ $0 == '1']
+    elif [ $0 == '1' ]
+    then
             go test -run Election
-        fi
-        if [ $0 == '2']
-            go test -run FailNoAgree
-        fi   
+    elif [ $0 == '2' ]
+    then
+            go test -run FailNoAgree   
     fi
 else
     go test -run Election
